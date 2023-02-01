5ff32b2c1a4977ce0cbc53824deff9140cfd7d8e
Alex Isaak <gafar@xaker.ru>
2023-02-01 18:53:30 +0300

    itogovy script

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+ 
+namespace kvadrat
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ведите число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ведите второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double kvad;
+            kvad = Math.Pow(a, 2);
+            if (b == kvad)
+            {
+                Console.WriteLine("b является квадратом числа a");
+            }
+            else
+            {
+                Console.WriteLine("b не является квадратом числа a");
+            }
+            
+        }
+    }
+}
